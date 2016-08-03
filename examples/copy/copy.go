c07d666da488df7d1d0e35ad019be29330cb244a
Ernst Rohlicek <ernst@rohlicek.at>
2016-08-03 22:29:46 +0200

    examples/copy: Add IIP support, use libflowd/iip (#45)

--- examples/copy/copy.go
+++ examples/copy/copy.go
@@ -4,19 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/ERnsTL/flowd/libflowd"
 )
 
 func main() {
-	// read list of output ports
+	// read list of output ports from program arguments
 	outPorts := os.Args[1:]
 	if len(outPorts) == 0 {
-		fmt.Println("ERROR: no output port names given")
-		os.Exit(1)
+		/*
+			fmt.Println("ERROR: no output port names given")
+			os.Exit(1)
+		*/
+		// get configuration from IIP = initial information packet/frame
+		fmt.Fprintln(os.Stderr, "copy: wait for IIP")
+		if iip, err := flowd.GetIIP("CONF"); err != nil {
+			fmt.Fprintln(os.Stderr, "ERROR getting IIP:", err, "- Exiting.")
+			os.Exit(1)
+		} else {
+			outPorts = strings.Split(iip, ",")
+			if len(outPorts) == 0 {
+				fmt.Fprintln(os.Stderr, "ERROR: no output ports names given in IIP, format is [port],[port],[port]...")
+				os.Exit(1)
+			}
+		}
 	}
+	fmt.Fprintln(os.Stderr, "copy: got output ports ", outPorts)
 
-	var frame *flowd.Frame
+	var frame *flowd.Frame //TDOO why is this pointer to Frame?
 	var err error
 	bufr := bufio.NewReader(os.Stdin)
 
