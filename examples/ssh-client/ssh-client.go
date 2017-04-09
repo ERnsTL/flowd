52a21549c3f4a559e1c5e15c9a8f659a9a837649
Ernst Rohlicek <ernst@rohlicek.at>
2017-04-09 22:16:46 +0200

    Add ssh-client component using Go package (closes #7)

--- examples/ssh-client/ssh-client.go
+++ examples/ssh-client/ssh-client.go
@@ -13,9 +13,7 @@ import (
 	"net"
 	"net/url"
 	"os"
-	"os/exec"
 	"strings"
-	"time"
 
 	"github.com/kballard/go-shellquote"
 	"golang.org/x/crypto/ssh"
@@ -42,18 +40,20 @@ func main() {
 		os.Exit(1)
 	}
 	// parse IIP
-	var retry, bridge, useAgent bool
+	var retry, bridge, useAgent, subsystem bool
 	var pkPath, khPath, hostkeyLine string
 	var operatingMode OperatingMode
 	iipSplit, err := shellquote.Split(iip)
 	checkError(err)
+	//TODO add flag for subsystem -> session.RequestSubsystem instead of session.Run()
 	flags := flag.NewFlagSet("ssh-client", flag.ContinueOnError)
 	flags.Var(&operatingMode, "mode", "operating mode: one (command instance handling all IPs) or each (IP handled by new instance)")
 	flags.BoolVar(&useAgent, "agent", true, "use ssh-agent as key source")
 	flags.StringVar(&pkPath, "i", "", "use private key file, e.g. ~/.ssh/id_ed25519|ecdsa|rsa|dsa")
 	flags.StringVar(&khPath, "knownhosts", os.ExpandEnv("$HOME/.ssh/known_hosts"), "path to SSH known_hosts file containing known host keys, may select wrong algo if multiple")
 	flags.StringVar(&hostkeyLine, "hostkey", "", "use hostkey line for server verification, put in quotes, get it using ssh-keyscan or from /etc/ssh/ssh_host_*_key.pub, must be for algo actually used")
-	flags.BoolVar(&bridge, "bridge", true, "bridge mode, true = forward frames from/to FBP network, false = send frame body over SSH, frame data from SSH")
+	flags.BoolVar(&subsystem, "subsystem", false, "request invocation of a subsystem on the remote system; which, is specified as the remote command")
+	flags.BoolVar(&bridge, "bridge", false, "bridge mode, true = forward frames from/to FBP network, false = send frame body over SSH, frame data from SSH")
 	flags.BoolVar(&retry, "retry", false, "retry connection and try to reconnect")
 	flags.BoolVar(&debug, "debug", false, "give detailed event output")
 	flags.BoolVar(&quiet, "quiet", false, "no informational output except errors")
@@ -70,9 +70,9 @@ func main() {
 		flags.PrintDefaults()
 		os.Exit(2)
 	}
-	cmdArgs := flags.Args()[1:]
+	cmdargs := flags.Args()[1:]
 	if debug {
-		fmt.Fprintf(os.Stderr, "cmdArgs=%v\n", cmdArgs)
+		fmt.Fprintf(os.Stderr, "cmdargs=%v\n", cmdargs)
 	}
 
 	//TODO implement
@@ -80,13 +80,18 @@ func main() {
 		fmt.Fprintln(os.Stderr, "ERROR: flag -retry currently unimplemented - exiting.")
 		os.Exit(2)
 	}
+	//TODO implement
+	if subsystem {
+		fmt.Fprintln(os.Stderr, "ERROR: flag -s currently unimplemented - exiting.")
+		os.Exit(2)
+	}
 
 	// parse remote address as URL
 	//TODO parse something like ssh://user@sampleserver.com
 	// NOTE: add double slashes after semicolon
 	remoteURLStr := flags.Args()[0]
-	if !strings.HasPrefix(remoteURLStr, "ssh://") {
-		remoteURLStr = "ssh://" + remoteURLStr
+	if !strings.HasPrefix(remoteURLStr, "tcp") {
+		remoteURLStr = "tcp://" + remoteURLStr
 	}
 	remoteURL, err := url.ParseRequestURI(remoteURLStr)
 	checkError(err)
@@ -177,7 +182,7 @@ func main() {
 			// NOTE: need hostname without port and without [] braces, contrary to suggestions in many places
 			hostname = strings.SplitN(hostname, ":", 2)[0]
 			if debug {
-				fmt.Fprintf(os.Stderr, "hostkey callback shall check %s\n", hostname)
+				fmt.Fprintf(os.Stderr, "host key callback shall check host '%s'\n", hostname)
 			}
 			//hasher := sha1.New() // NOTE: hasher.Sum() returns []byte, but sha1.Sum() returns [size]byte
 			var foundHostKey ssh.PublicKey
@@ -238,7 +243,7 @@ func main() {
 
 			// check actual host key
 			if debug {
-				fmt.Fprintf(os.Stderr, "found host key, checking...\n")
+				fmt.Fprintf(os.Stderr, "host key found, checking...\n")
 			}
 			if !bytes.Equal(key.Marshal(), foundHostKey.Marshal()) {
 				return fmt.Errorf("known host key found for active key type, but mismatch")
@@ -259,7 +264,7 @@ func main() {
 	if !quiet {
 		fmt.Fprintln(os.Stderr, "connecting to server")
 	}
-	connServer, err := ssh.Dial("tcp", remoteHost, sshConfig)
+	connServer, err := ssh.Dial(remoteNetwork, remoteHost, sshConfig)
 	checkError(err)
 	defer connServer.Close()
 	//TODO unreachable defer agentConn.Close()
@@ -267,147 +272,38 @@ func main() {
 		fmt.Fprintln(os.Stderr, "connected")
 	}
 
-	// make session for remote command
-	// NOTE: each session accepts only one call to Run, Start, Shell, Output or CombinedOutput
-	started := time.Now()
-
-	if !quiet {
-		fmt.Fprintln(os.Stderr, "asking for new session")
-	}
-	session, err := connServer.NewSession()
-	checkError(err)
-	defer session.Close()
-	if !quiet {
-		fmt.Fprintln(os.Stderr, "session established")
-	}
-
-	// request pseudo-terminal (pty)
-	/*
-		terminalConfig := ssh.TerminalModes{
-			53:  0,     // echo off
-			128: 14400, // input speed = 14.4 kbaud
-			129: 14400, // output speed = 14.4 kbaud
-		}
-		err = session.RequestPty("xterm", 80, 40, terminalConfig)
-		checkError(err)
-	*/
-
-	// prepare for exec request
-	cmdOut := new(bytes.Buffer)
-	cmdErr := new(bytes.Buffer)
-	session.Stdout = cmdOut
-	session.Stderr = cmdErr
-	if !quiet {
-		fmt.Fprintf(os.Stderr, "starting remote command %v\n", cmdArgs)
-	}
-	// NOTE: Run = Start + Wait
-	// TODO optimize - first split in IIP and flags, then joined here
-	cmdArgsStr := strings.Join(cmdArgs, " ")
-	err = session.Run(cmdArgsStr)
-	runtime := time.Now().Sub(started).Seconds()
-	if !quiet {
-		fmt.Fprintf(os.Stderr, "runtime of request was %.03f sec\n", runtime)
-	}
-	// check exit status
-	var exitCode int
-	/* TODO maybe only exit status is interesting
-	if exitError, ok := r.Error.(*ssh.ExitError); ok {
-		r.ExitStatus = exitError.ExitStatus()
-	}
-	*/
-	if err != nil {
-		switch err := err.(type) {
-		case *ssh.ExitError:
-			exitCode = err.ExitStatus()
-		case *ssh.ExitMissingError:
-			if debug {
-				fmt.Fprintln(os.Stderr, "program exited, error status missing")
-			}
-		default:
-			// TODO check detailed for IO errors, check ssh package source code
-			fmt.Fprintf(os.Stderr, "other error occured: %v\n", err)
-		}
-	}
-	if exitCode != 0 {
-		if !quiet {
-			fmt.Fprintf(os.Stderr, "program exited with status code %d\n", exitCode)
-		}
-	} else {
-		if !quiet {
-			fmt.Fprintln(os.Stderr, "program exited normally")
-		}
-	}
-
-	// print output
-	if debug {
-		fmt.Fprintf(os.Stderr, "program STDOUT result: %s\n", cmdOut.String())
-		fmt.Fprintf(os.Stderr, "program STDERR result: %s\n", cmdErr.String())
-	}
-
-	if debug {
-		stats := map[string]string{
-			"stdout":  fmt.Sprintf("%d bytes", len(cmdOut.String())),
-			"stderr":  fmt.Sprintf("%d bytes", len(cmdErr.String())),
-			"runtime": fmt.Sprintf("%0.3f", runtime),
-			"status":  fmt.Sprintf("%d", exitCode),
-		}
-		fmt.Fprintf(os.Stderr, "%+v\n", stats)
-	}
-
-	fmt.Fprintln(os.Stderr, "done with SSH call")
-
-	//--- end SSH client
-
-	// prepare remote subprocess variables
-	var cin io.WriteCloser
-	var cout io.ReadCloser
-
 	//TODO implement timeout on subprocess
-	/*
-		// start
-		cmd := exec.Command("sleep", "5")
-		if err := cmd.Start(); err != nil {
-			panic(err)
-		}
-
-		// wait or timeout
-		donec := make(chan error, 1)
-		go func() {
-			donec <- cmd.Wait()
-		}()
-		select {
-		case <-time.After(3 * time.Second):
-			cmd.Process.Kill()
-			fmt.Println("timeout")
-		case <-donec:
-			fmt.Println("done")
-		}
-	*/
 	//TODO implement retry/restart in one mode
 	//TODO implement retry/restart in each mode
 
 	// main work loops
+	var session *ssh.Session
+	var cin io.WriteCloser
+	var cout io.Reader
 	switch operatingMode {
 	case One:
 		// start command as subprocess, with arguments
-		cin, cout = startCommand(cmdargs)
-		defer cout.Close()
-		defer cin.Close()
+		session, cin, cout = startCommand(connServer, cmdargs)
+		//defer cin.Close()
 
 		// handle subprocess output
-		go handleCommandOutput(debug, cout)
+		if bridge {
+			go handleCommandOutput(debug, cout)
+		} else {
+			go handleCommandOutputRaw(debug, cout)
+		}
 
 		// handle subprocess input
-		if bridge == true {
+		if bridge {
 			// setup direct copy without processing (since already framed)
-			if _, err := io.Copy(cin, bufr); err != nil {
+			if _, err := io.Copy(cin, stdin); err != nil {
 				fmt.Fprintln(os.Stderr, "ERROR: receiving from FBP network:", err, "Closing.")
 				os.Stdin.Close()
 				return
 			}
 		} else {
 			// loop: read frame, write body to subprocess
-			copyFrameBodies(cin, bufr)
+			copyFrameBodies(cin, stdin)
 		}
 	case Each:
 		// prepare variables
@@ -416,7 +312,7 @@ func main() {
 
 		for {
 			// read frame
-			frame, err = flowd.ParseFrame(bufr)
+			frame, err = flowd.ParseFrame(stdin)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
 			} else if debug {
@@ -426,24 +322,24 @@ func main() {
 			// handle each frame with a new instance
 			go func(frame *flowd.Frame) {
 				// start new command instance
-				cmd, cin, cout = startCommand(cmdargs)
+				session, cin, cout = startCommand(connServer, cmdargs)
 
 				// handle subprocess output
-				if framing {
+				if bridge {
 					go handleCommandOutput(debug, cout)
 				} else {
 					go handleCommandOutputRaw(debug, cout)
 				}
 
 				// forward frame or frame body to subprocess
-				if framing {
-					// frame
+				if bridge {
+					// write frame
 					if err := frame.Marshal(cin); err != nil {
 						fmt.Fprintln(os.Stderr, "ERROR: marshaling frame to command STDIN:", err, "- Exiting.")
 						os.Exit(3)
 					}
 				} else {
-					// frame body
+					// write frame body
 					if _, err := cin.Write(frame.Body); err != nil {
 						fmt.Fprintln(os.Stderr, "ERROR: writing frame body to command STDIN:", err, "- Exiting.")
 						os.Exit(3)
@@ -458,9 +354,14 @@ func main() {
 				}
 
 				// wait for subprocess to finish
-				if err := cmd.Wait(); err != nil {
+				exitStatus := session.Wait()
+				if exitCode, err := getExitCode(exitStatus); err != nil {
 					fmt.Fprintln(os.Stderr, "ERROR: command exited with error:", err, "- Exiting.")
 					os.Exit(3)
+				} else {
+					if !quiet {
+						fmt.Fprintln(os.Stderr, "command exited with code", exitCode)
+					}
 				}
 			}(frame)
 		}
@@ -481,29 +382,97 @@ func printUsage() {
 	fmt.Fprintln(os.Stderr, "IIP format: [flags] [ssh://][user=me]:[password]@[host][:port=22] [cmdpath] [args...]")
 }
 
-//TODO pretty much 1:1 copy from cmd startCommand() -> reuse?
-func startCommand(cmdargs []string) (cmd *exec.Cmd, cin io.WriteCloser, cout io.ReadCloser) {
+func startCommand(connServer *ssh.Client, cmdargs []string) (session *ssh.Session, cin io.WriteCloser, cout io.Reader) {
+	// make session for remote command
+	// NOTE: each session accepts only one call to Run, Start, Shell, Output or CombinedOutput
+	if !quiet {
+		fmt.Fprintln(os.Stderr, "asking for new session")
+	}
 	var err error
-	cmd = exec.Command(cmdargs[0], cmdargs[1:]...)
-	cout, err = cmd.StdoutPipe()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "ERROR: could not allocate pipe from command stdout:", err)
+	session, err = connServer.NewSession()
+	checkError(err)
+	//defer session.Close()	- must not do this here in this function
+	if !quiet {
+		fmt.Fprintln(os.Stderr, "session established")
 	}
-	cin, err = cmd.StdinPipe()
+
+	// request pseudo-terminal (pty)
+	/*
+		terminalConfig := ssh.TerminalModes{
+			53:  0,     // echo off
+			128: 14400, // input speed = 14.4 kbaud
+			129: 14400, // output speed = 14.4 kbaud
+		}
+		err = session.RequestPty("xterm", 80, 40, terminalConfig)
+		checkError(err)
+	*/
+
+	// prepare stdin, stdout and stderr targets
+	//cout, coutw, err := os.Pipe()
+	cout, err = session.StdoutPipe()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "ERROR: could not allocate pipe to command stdin:", err)
+		fmt.Fprintln(os.Stderr, "ERROR: could not allocate pipe from session stdout:", err)
 	}
-	cmd.Stderr = os.Stderr
-	err = cmd.Start()
+	//session.Stdout = coutw
+	cin, err = session.StdinPipe()
+	//cinr, cin, err := os.Pipe()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "ERROR: starting command:", err)
-		os.Exit(3)
+		fmt.Fprintln(os.Stderr, "ERROR: could not allocate pipe to session stdin:", err)
 	}
+	//session.Stdin = cinr
+	session.Stderr = os.Stderr
+
+	// start the remote command
+	// NOTE: Run = Start + Wait
+	// TODO optimize - first split in IIP and flags, then joined here
+	if !quiet {
+		fmt.Fprintf(os.Stderr, "starting remote command %v\n", cmdargs)
+	}
+	cmdargsStr := strings.Join(cmdargs, " ")
+	err = session.Start(cmdargsStr)
+	checkError(err)
+
 	return
 }
 
+//TODO optimize code or make it unnecessary
+func getExitCode(err error) (int, error) {
+	// check exit status
+	var exitCode int
+	/* TODO maybe only exit status is interesting
+	if exitError, ok := r.Error.(*ssh.ExitError); ok {
+		r.ExitStatus = exitError.ExitStatus()
+	}
+	*/
+	if err != nil {
+		switch err := err.(type) {
+		case *ssh.ExitError:
+			exitCode = err.ExitStatus()
+		case *ssh.ExitMissingError:
+			if debug {
+				fmt.Fprintln(os.Stderr, "program exited, error status missing")
+			}
+			return -1, fmt.Errorf("program exited, error status missing")
+		default:
+			// TODO check detailed for IO errors, check ssh package source code
+			fmt.Fprintf(os.Stderr, "other error occured: %v\n", err)
+			return -1, err
+		}
+	}
+	if exitCode != 0 {
+		if !quiet {
+			fmt.Fprintf(os.Stderr, "program exited with status code %d\n", exitCode)
+		}
+	} else {
+		if !quiet {
+			fmt.Fprintln(os.Stderr, "program exited normally")
+		}
+	}
+	return exitCode, nil
+}
+
 //TODO pretty much 1:1 copy from cmd handleCommandOutput() -> reuse?
-func handleCommandOutput(debug bool, cout io.ReadCloser) {
+func handleCommandOutput(debug bool, cout io.Reader) {
 	bufr := bufio.NewReader(cout)
 	for {
 		frame, err := flowd.ParseFrame(bufr)
@@ -513,7 +482,7 @@ func handleCommandOutput(debug bool, cout io.ReadCloser) {
 			} else {
 				fmt.Fprintln(os.Stderr, "ERROR parsing frame from command stdout:", err, "- Exiting.")
 			}
-			cout.Close()
+			//cout.Close()
 			return
 		}
 
@@ -533,7 +502,7 @@ func handleCommandOutput(debug bool, cout io.ReadCloser) {
 }
 
 //TODO pretty much 1:1 copy from cmd handleCommandOutputRaw() -> reuse?
-func handleCommandOutputRaw(debug bool, cout io.ReadCloser) {
+func handleCommandOutputRaw(debug bool, cout io.Reader) {
 	// prepare readers and variables
 	bufr := bufio.NewReader(cout)
 	buf := make([]byte, bufSize)
@@ -547,13 +516,14 @@ func handleCommandOutputRaw(debug bool, cout io.ReadCloser) {
 		nbytes, err := bufr.Read(buf)
 		if err != nil {
 			if err == io.EOF {
+				//TODO that is actually normal behavior in mode=each
 				fmt.Fprintln(os.Stderr, "WARNING: EOF from command:", err, "- Closing.")
-				cout.Close()
+				//cout.Close()
 				return
 			}
 			// other error
 			fmt.Fprintln(os.Stderr, "ERROR: reading from command STDOUT:", err, "- Closing.")
-			cout.Close()
+			//cout.Close()
 			return
 		}
 
