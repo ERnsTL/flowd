671205c099492f1a01df0d3d87a8c577d69134c1
Ernst Rohlicek <ernst@rohlicek.at>
2017-05-31 00:30:34 +0200

    Add basic sort component with example (closes #35)
    examples/sort: Add basic sort component
    examples/sortlines.fbp: Add example network using sort and file components

---
+++ examples/sort/sort.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+
+	"github.com/ERnsTL/flowd/libflowd"
+)
+
+func main() {
+	// open connection to network
+	bufr := bufio.NewReader(os.Stdin)
+	fmt.Fprintln(os.Stderr, "reading packets")
+
+	//TODO add configuration to do numeric sort
+	//TODO maybe add sorting of substreams = bracketed groups
+
+	var frame *flowd.Frame //TODO why is this pointer to Frame?
+	var err error
+	var toSort []string
+
+	// read frames
+	for {
+		frame, err = flowd.ParseFrame(bufr)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		// check for port close notification
+		if frame.Type == "control" && frame.BodyType == "PortClose" {
+			fmt.Fprintln(os.Stderr, "got all messages")
+			// done
+			break
+		}
+
+		// append to list
+		toSort = append(toSort, string(frame.Body))
+	}
+
+	// sort
+	fmt.Fprintln(os.Stderr, "sorting")
+	sort.Strings(toSort)
+
+	// send out sorted
+	fmt.Fprintln(os.Stderr, "sending out sorted")
+	outframe := flowd.Frame{
+		Type:     "data",
+		BodyType: "Sorted",
+		Port:     "OUT",
+	}
+	for i := 0; i < len(toSort); i++ {
+		outframe.Body = []byte(toSort[i])
+		if err := outframe.Marshal(os.Stdout); err != nil {
+			fmt.Fprintln(os.Stderr, "ERROR: marshaling frame:", err.Error())
+		}
+	}
+
+	// report
+	fmt.Fprintln(os.Stderr, "all sent, exiting.")
+}
