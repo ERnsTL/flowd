d787dc9e453e9323fdc513d6bb6c89065c0ae840
Ernst Rohlicek <ernst@rohlicek.at>
2017-06-05 19:05:34 +0200

    examples/concatenate: Add component draining inports and forwarding in order (closes #88) examples/examples/concatenate-files.fbp: Add example network for concatenate component

---
+++ examples/concatenate/concatenate.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/ERnsTL/flowd/libflowd"
+)
+
+func main() {
+	// open connection to network
+	bufr := bufio.NewReader(os.Stdin)
+	// get configuration from IIP = initial information packet/frame
+	var inPorts []string
+	fmt.Fprintln(os.Stderr, "wait for IIP")
+	if iip, err := flowd.GetIIP("CONF", bufr); err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR getting IIP:", err, "- Exiting.")
+		os.Exit(1)
+	} else {
+		inPorts = strings.Split(iip, ",")
+		if len(inPorts) == 0 {
+			fmt.Fprintln(os.Stderr, "ERROR: no input ports names given in IIP, format is [port],[port],[port]...")
+			os.Exit(1)
+		}
+	}
+	fmt.Fprintln(os.Stderr, "got input ports", inPorts)
+
+	// prepare variables
+	var frame *flowd.Frame
+	var err error
+	var frameBuffer []*flowd.Frame
+
+	// for each specified input port...
+forPorts:
+	for portIndex, inPort := range inPorts {
+		//TODO if debug fmt.Fprintf(os.Stderr, "inPort=%s\n", inPort)
+		// catch up any buffered IPs for new input port, in order
+		for bufferIndex, frame := range frameBuffer {
+			if frame.Port == inPort {
+				// check for port close notification
+				if frame.Type == "control" && frame.BodyType == "PortClose" {
+					// done with this input port
+					fmt.Fprintln(os.Stderr, "done with "+inPort)
+					// remove it from buffer
+					// source: https://github.com/golang/go/wiki/SliceTricks
+					copy(frameBuffer[bufferIndex:], frameBuffer[bufferIndex+1:])
+					// if last input port, then exit
+					if portIndex == len(inPorts)-1 {
+						//TODO if debug fmt.Fprintln(os.Stderr, "this was last input port")
+						break forPorts
+					}
+					break
+				}
+				//TODO if debug fmt.Fprintln(os.Stderr, "sending it on")
+				// send it to output port
+				frame.Port = "OUT"
+				if err = frame.Marshal(os.Stdout); err != nil {
+					fmt.Fprintln(os.Stderr, "ERROR: marshaling frame:", err.Error())
+				}
+				// remove it from buffer
+				copy(frameBuffer[bufferIndex:], frameBuffer[bufferIndex+1:])
+			}
+		}
+
+		// read new frames
+		for {
+			// read frame
+			frame, err = flowd.ParseFrame(bufr)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+
+			// if from current input port
+			//TODO if debug fmt.Fprintln(os.Stderr, "got IP on input port "+frame.Port+"; inPort="+inPort)
+			if frame.Port == inPort {
+				// check for port close notification
+				if frame.Type == "control" && frame.BodyType == "PortClose" {
+					// done with this input port
+					fmt.Fprintln(os.Stderr, "done with "+inPort)
+					break
+				}
+				// send it to output port
+				//TODO if debug fmt.Fprintln(os.Stderr, "sending it on")
+				frame.Port = "OUT"
+				if err := frame.Marshal(os.Stdout); err != nil {
+					fmt.Fprintln(os.Stderr, "ERROR: marshaling frame:", err.Error())
+				}
+			} else {
+				// store it for later, in order
+				//TODO if debug fmt.Fprintln(os.Stderr, "buffering it")
+				frameBuffer = append(frameBuffer, frame)
+			}
+		}
+	}
+
+	// report
+	fmt.Fprintln(os.Stderr, "exiting.")
+}
