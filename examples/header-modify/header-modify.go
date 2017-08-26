c09812457f86ceaa0e5e1e3b403687528f648521
Ernst Rohlicek <ernst@rohlicek.at>
2017-08-26 17:16:08 +0200

    examples/header-modify: Add appending and prepending, quoting, fixes
    examples/header-modify: Change modifications from map to []modFunc
    examples/header-modify: Split IIP using go-shellquote, then parse flags
    examples/header-modify: printUsage(): New function with detailed info, for flags
    examples/header-modify: Add -debug and -quiet flags
    examples/header-modify: Adjust status output according to flags
    examples/header-modify: Add ability to give multiple modification specifications
    examples/header-modify: Fix regex for modification specification parsing
    examples/header-modify: Add modification functions to abstract over different ones
    examples/header-modify: Apply modifications in order on each incoming frame

--- examples/header-modify/header-modify.go
+++ examples/header-modify/header-modify.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"regexp"
 	"time"
 
 	"github.com/ERnsTL/flowd/libflowd"
+	shellquote "github.com/kballard/go-shellquote"
 )
 
 const maxFlushWait = 5 * time.Second // duration to wait until forcing STDOUT flush
@@ -27,35 +29,119 @@ func main() {
 		}
 	}()
 	// get configuration from IIP = initial information packet/frame
-	modifications := map[string]string{}
+	type modFunc func(*flowd.Frame)
+	modifications := []modFunc{}
 	fmt.Fprintln(os.Stderr, "wait for IIP")
 	if iip, err := flowd.GetIIP("CONF", netin); err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR getting IIP:", err, "- Exiting.")
 		os.Exit(1)
 	} else {
-		if len(iip) == 0 {
-			fmt.Fprintln(os.Stderr, "ERROR: received empty IIP, format is [field]=[value], eg. BodyType=Message Extension-Field=1")
-			os.Exit(1)
+		// split into arguments, respecting quoted multi-word arguments
+		iipSplit, err := shellquote.Split(iip)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "ERROR: parsing IIP:", err)
+			os.Exit(2)
 		}
+		var debug, quiet bool
+		flags := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+		flags.BoolVar(&debug, "debug", false, "give detailed event output")
+		flags.BoolVar(&quiet, "quiet", false, "no informational output except errors")
+		if err := flags.Parse(iipSplit); err != nil {
+			os.Exit(2)
+		}
+
+		// check flags
+		if flags.NArg() == 0 {
+			fmt.Fprintln(os.Stderr, "ERROR: missing modification specification(s) in free argument(s)")
+			printUsage()
+			flags.PrintDefaults() // prints to STDERR
+			os.Exit(2)
+		}
+		if !quiet {
+			fmt.Fprintf(os.Stderr, "got %d modification specification(s)\n", flags.NArg())
+		}
+
 		// parse specifications
 		//TODO add ability to set BodyType non-extension field
 		//TODO add ability to set field to ""
 		//TODO add ability to delete header field
-		re := regexp.MustCompile("(.+)=(.*)")
-		args := re.FindAllStringSubmatch(iip, -1)
-		if args == nil {
-			fmt.Fprintln(os.Stderr, "ERROR: no modification specification(s) found")
-			fmt.Fprintln(os.Stderr, "IIP format: [field]=[value]...")
-			os.Exit(1)
-		}
-		for _, arg := range args {
-			field := arg[1]
-			value := arg[2]
-			//TODO if debug fmt.Fprintf(os.Stderr, "got modification %s to %s\n", field, value)
-			modifications[field] = value
+		//TODO maybe add alternative append and prepend operator in case + needs to be in the value
+		// NOTE: U: = non-capturing flag "un-greedy"
+		re := regexp.MustCompile("(?U:(.+))(\\+=|=\\+|=)(.*)")
+		for _, arg := range flags.Args() {
+			if debug {
+				fmt.Fprintln(os.Stderr, "got argument:", arg)
+			}
+			spec := re.FindStringSubmatch(arg)
+			if spec == nil {
+				// no match, display usage information
+				printUsage()
+				os.Exit(2)
+			}
+			field := spec[1]
+			op := spec[2]
+			value := spec[3]
+			if debug {
+				fmt.Fprintln(os.Stderr, "parsed argument into specification:", spec)
+			}
+			switch op {
+			case "=":
+				// set value; add according function to modification list
+				if debug {
+					fmt.Fprintf(os.Stderr, "\tset %s to %s\n", field, value)
+				}
+				modifications = append(modifications, func(frame *flowd.Frame) {
+					if frame.Extensions == nil {
+						frame.Extensions = map[string]string{
+							field: value,
+						}
+					} else {
+						frame.Extensions[field] = value
+					}
+					/*TODO optimize: which version is more performant? beter for CPU branch predictor?
+					if frame.Extensions == nil {
+						frame.Extensions = map[string]string{}
+					}
+					frame.Extensions[field] = value
+					*/
+					fmt.Fprintf(os.Stderr, "\tsetting %s to %s\n", field, value)
+				})
+			case "+=":
+				// append value, if field exists
+				if debug {
+					fmt.Fprintf(os.Stderr, "\tappend %s to %s\n", value, field)
+				}
+				modifications = append(modifications, func(frame *flowd.Frame) {
+					if frame.Extensions != nil {
+						if _, found := frame.Extensions[field]; found {
+							fmt.Fprintf(os.Stderr, "\tappending %s to %s\n", value, field)
+							frame.Extensions[field] += value
+							return
+						}
+					}
+					fmt.Fprintf(os.Stderr, "\t%s missing, leaving unmodified\n", field)
+				})
+			case "=+":
+				// prepend, if field exists
+				if debug {
+					fmt.Fprintf(os.Stderr, "\tprepend %s to %s\n", value, field)
+				}
+				modifications = append(modifications, func(frame *flowd.Frame) {
+					if frame.Extensions != nil {
+						if currentValue, found := frame.Extensions[field]; found {
+							fmt.Fprintf(os.Stderr, "\tprepending %s to %s\n", value, field)
+							frame.Extensions[field] = value + currentValue
+							return
+						}
+					}
+					fmt.Fprintf(os.Stderr, "\t%s missing, leaving unmodified\n", field)
+				})
+			default:
+				fmt.Fprintln(os.Stderr, "ERROR: unexpected operation:", op, "- Exiting.")
+				os.Exit(2)
+			}
 		}
 	}
-	fmt.Fprintf(os.Stderr, "got %d modification specification(s)\n", len(modifications))
 
 	// prepare variables
 	var frame *flowd.Frame
@@ -78,12 +164,8 @@ func main() {
 		// apply modification(s)
 		//TODO make this configurable using "debug" parameter in IIP
 		fmt.Fprintln(os.Stderr, "got frame, modifying...")
-		if frame.Extensions == nil {
-			frame.Extensions = map[string]string{}
-		}
-		for field, value := range modifications {
-			fmt.Fprintf(os.Stderr, "\tsetting %s to %s\n", field, value)
-			frame.Extensions[field] = value
+		for _, ruleFunc := range modifications {
+			ruleFunc(frame)
 		}
 		fmt.Fprintln(os.Stderr, "modified. forwarding now.")
 
@@ -93,4 +175,10 @@ func main() {
 			fmt.Fprintln(os.Stderr, "ERROR: marshaling frame:", err.Error())
 		}
 	}
+}
+
+func printUsage() {
+	fmt.Fprintln(os.Stderr, "IIP format: [field][=|+=|=+][value]...")
+	fmt.Fprintln(os.Stderr, "= sets, += appends, =+ prepends the value to the given field")
+	fmt.Fprintln(os.Stderr, "multiple specifications possible, order guaranteed; possible to quote a specification")
 }
