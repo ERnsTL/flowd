3b8ab14c36e4c24d80171a968b318a86d1a95bce
Ernst Rohlicek <ernst@rohlicek.at>
2017-08-26 17:07:27 +0200

    examples/counter: Add counting per occurrence of field value
    examples/counter: Add flag -packetsperfieldvalue
    examples/counter: Move global variable near beginning of source file
    examples/counter: Add counter interface to abstract over different counts
    examples/counter: Add packet counter implementation
    examples/counter: Add total frame size counter implementation
    examples/counter: Add packets per field value counter implementation
    examples/counter: Initialize correct counter depending on flags
    examples/counter: Count only what is asked for, not everything
    examples/counter: Improve counter reporting and count increases

--- examples/counter/counter.go
+++ examples/counter/counter.go
@@ -13,6 +13,13 @@ import (
 
 var (
 	debug, quiet bool
+	// prepared frame
+	countFrame = &flowd.Frame{
+		Type:     "data",
+		BodyType: "Count",
+		Port:     "OUT",
+	}
+	netout *bufio.Writer //TODO is concurrent use OK in this case here?
 )
 
 func main() {
@@ -22,6 +29,9 @@ func main() {
 	defer netout.Flush()
 	// flag variables
 	var brackets, control bool
+	var packets, size bool
+	var packetsPerFieldValue string
+
 	// get configuration from IIP = initial information packet/frame
 	fmt.Fprintln(os.Stderr, "wait for IIP")
 	if iip, err := flowd.GetIIP("CONF", netin); err != nil {
@@ -34,6 +44,7 @@ func main() {
 		flags.BoolVar(&control, "control", false, "count control packets as well")
 		flags.BoolVar(&size, "size", false, "count size of packet bodies")
 		flags.BoolVar(&packets, "packets", false, "count number of packets")
+		flags.StringVar(&packetsPerFieldValue, "packetsperfieldvalue", "", "count number of packets per value of given header field")
 		flags.BoolVar(&debug, "debug", false, "give detailed event output")
 		flags.BoolVar(&quiet, "quiet", false, "no informational output except errors")
 		if err := flags.Parse(strings.Split(iip, " ")); err != nil {
@@ -44,12 +55,38 @@ func main() {
 			flags.PrintDefaults() // prints to STDERR
 			os.Exit(2)
 		}
-		if (!size && !packets) || (size && packets) {
-			fmt.Fprintln(os.Stderr, "ERROR: either -size or -packets expected - unable to proceed")
+		countersGiven := 0
+		if size {
+			countersGiven++
+		}
+		if packets {
+			countersGiven++
+		}
+		if packetsPerFieldValue != "" {
+			countersGiven++
+		}
+		if countersGiven != 1 {
+			fmt.Fprintln(os.Stderr, "ERROR: either -size, -packets or -packetsperfieldvalue expected - unable to proceed")
 			//TODO add this printUsage()
 			flags.PrintDefaults() // prints to STDERR
 			os.Exit(2)
 		}
+	}
+
+	// prepare according counter
+	var counter counter
+	if packets {
+		counter = &packetCounter{}
+	} else if size {
+		counter = &sizeCounter{}
+	} else if packetsPerFieldValue != "" {
+		counter = &packetsPerFieldValueCounter{
+			Field:               packetsPerFieldValue,
+			countsPerFieldValue: map[string]int{},
+		}
+	} else {
+		fmt.Fprintln(os.Stderr, "ERROR: unknown counter type!")
+		os.Exit(2)
 	}
 
 	var frame *flowd.Frame
@@ -65,23 +102,22 @@ func main() {
 		// handle control frames
 		if frame.Type == "control" {
 			if control {
-				packetCount++
-				packetSize += len(frame.Body)
+				counter.Count(frame)
 			}
 			switch frame.BodyType {
 			case "BracketOpen":
 				if brackets {
-					packetCount = 0
+					counter.Reset()
 				}
 			case "BracketClose":
 				if brackets {
-					sendCount()
+					counter.Report()
 				}
 			case "PortClose":
 				if frame.Port == "IN" {
 					fmt.Fprintln(os.Stderr, "got port close notification - sending final count before exiting.")
 					// send final count
-					sendCount()
+					counter.Report()
 					// exit
 					// NOTE: os.Exit() would prevent the deferred netout.Flush()
 					return
@@ -94,58 +130,138 @@ func main() {
 				if !quiet {
 					fmt.Fprintln(os.Stderr, "resetting count as requested")
 				}
-				packetCount = 0
-				packetSize = 0
+				counter.Reset()
 				continue
 			} else if frame.Port == "REPORT" {
 				// report current count value
 				if !quiet {
 					fmt.Fprintln(os.Stderr, "reporting count as requested")
 				}
-				sendCount()
+				counter.Report()
 				// send immediately; presumably a report is not requested too often
 				netout.Flush()
 				continue
 			}
 
 			// regular case and frame
-			packetCount++
-			packetSize += len(frame.Body)
-			if debug {
-				fmt.Fprintf(os.Stderr, "increased packet count to %d, size count to %d\n", packetCount, packetSize)
-			}
+			counter.Count(frame)
 		}
 	}
 }
 
-// NOTE: used for optimal (? TODO) transfer of information to sendCount
-var (
-	packets     bool
-	size        bool
-	packetCount int
-	packetSize  int
-	countFrame  = &flowd.Frame{
-		Type:     "data",
-		BodyType: "PacketCount",
-		Port:     "OUT",
-	}
-	netout *bufio.Writer //TODO is concurrent use OK in this case here?
-)
+type counter interface {
+	Count(*flowd.Frame)
+	Report()
+	Reset()
+}
 
-func sendCount() {
-	// send count to output port
-	if packets {
-		if debug {
-			fmt.Fprintf(os.Stderr, "sending packet count of %d\n", packetCount)
-		}
-		countFrame.Body = []byte(strconv.Itoa(packetCount))
-	} else {
-		if debug {
-			fmt.Fprintf(os.Stderr, "sending size count of %d\n", packetSize)
-		}
-		countFrame.Body = []byte(strconv.Itoa(packetSize))
+type packetCounter struct {
+	packetCount int
+}
+
+func (c *packetCounter) Count(frame *flowd.Frame) {
+	c.packetCount++
+	if !quiet {
+		fmt.Fprintln(os.Stderr, "increased packet count to", c.packetCount)
 	}
+}
+
+func (c *packetCounter) Report() {
+	// fill count frame
+	if !quiet {
+		fmt.Fprintln(os.Stderr, "sending packet count of", c.packetCount)
+	}
+	countFrame.Body = []byte(strconv.Itoa(c.packetCount))
+	// send report to FBP network
 	if err := countFrame.Marshal(netout); err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR: marshaling frame:", err.Error())
 	}
+}
+
+func (c *packetCounter) Reset() {
+	c.packetCount = 0
+}
+
+type sizeCounter struct {
+	totalSize int
+}
+
+func (c *sizeCounter) Count(frame *flowd.Frame) {
+	c.totalSize += len(frame.Body)
+	if !quiet {
+		fmt.Fprintln(os.Stderr, "increased total packet size to", c.totalSize)
+	}
+}
+
+func (c *sizeCounter) Report() {
+	// fill count frame
+	if !quiet {
+		fmt.Fprintln(os.Stderr, "sending total size of", c.totalSize)
+	}
+	countFrame.Body = []byte(strconv.Itoa(c.totalSize))
+	// send report to FBP network
+	if err := countFrame.Marshal(netout); err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR: marshaling frame:", err.Error())
+	}
+}
+
+func (c *sizeCounter) Reset() {
+	c.totalSize = 0
+}
+
+type packetsPerFieldValueCounter struct {
+	Field               string
+	countsPerFieldValue map[string]int
+}
+
+func (c *packetsPerFieldValueCounter) Count(frame *flowd.Frame) {
+	if frame.Extensions != nil {
+		if valueInFrame, exists := frame.Extensions[c.Field]; exists {
+			if _, exists := c.countsPerFieldValue[valueInFrame]; exists {
+				// increase value
+				c.countsPerFieldValue[valueInFrame]++
+			} else {
+				// make new entry with initial value
+				c.countsPerFieldValue[valueInFrame] = 1
+			}
+			if !quiet {
+				fmt.Fprintf(os.Stderr, "increased packet count for %s=%s to %d\n", c.Field, valueInFrame, c.countsPerFieldValue[valueInFrame])
+			}
+		} else {
+			// given field not found in frame, nothing to count
+			if !quiet {
+				fmt.Fprintln(os.Stderr, c.Field, "not found, count unchanged")
+			}
+		}
+	} else {
+		// no extensions found, nothing to count
+		if !quiet {
+			fmt.Fprintln(os.Stderr, c.Field, "not found, count unchanged")
+		}
+	}
+}
+
+func (c *packetsPerFieldValueCounter) Report() {
+	if !quiet {
+		fmt.Fprintln(os.Stderr, "sending packet counts per values of field", c.Field)
+	}
+	for fieldValue, count := range c.countsPerFieldValue {
+		if !quiet {
+			fmt.Fprintf(os.Stderr, "\t%s=%s appeared %d times\n", c.Field, fieldValue, count)
+		}
+		// fill count frame
+		countFrame.Extensions = map[string]string{
+			c.Field: fieldValue,
+		}
+		///TODO if above works, remove - countFrame.Extensions[c.Field] = fieldValue
+		countFrame.Body = []byte(strconv.Itoa(count))
+		// send report to FBP network
+		if err := countFrame.Marshal(netout); err != nil {
+			fmt.Fprintln(os.Stderr, "ERROR: marshaling frame:", err.Error())
+		}
+	}
+}
+
+func (c *packetsPerFieldValueCounter) Reset() {
+	c.countsPerFieldValue = map[string]int{}
 }
