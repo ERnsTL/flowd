4e658e94e4d902bce57425767adfe574123704a3
Ernst Rohlicek <ernst@rohlicek.at>
2019-08-09 14:24:51 +0200

    components/copy: Add useful tip in argument parsing error message components/copy: Add conditional flushing of netout based on input queue components/copy: Optimize conditional flushing

--- components/copy/copy.go
+++ components/copy/copy.go
@@ -16,7 +16,7 @@ func main() {
 	unixfbp.DefFlags()
 	flag.Parse()
 	if flag.NArg() != 0 {
-		fmt.Fprintln(os.Stderr, "ERROR: unexpected free arguments given")
+		fmt.Fprintln(os.Stderr, "ERROR: unexpected free arguments given (outports are auto-detected)")
 		flag.PrintDefaults() // prints to STDERR
 		os.Exit(2)
 	}
@@ -48,6 +48,7 @@ func main() {
 
 	// main loop
 	var frame *flowd.Frame
+	var buffered int
 	for {
 		// read frame
 		frame, err = flowd.Deserialize(netin)
@@ -63,16 +64,19 @@ func main() {
 		}
 
 		// send it to given output ports
+		buffered = netin.Buffered()
 		for _, outPort := range unixfbp.OutPorts {
 			//frame.Port = outPort
 			if err = frame.Serialize(outPort.Writer); err != nil {
 				//TODO handle EOF gracefully = remove from list of ouptorts and continue as usual; if len(outports) == 0 then break
 				fmt.Fprintln(os.Stderr, "ERROR: marshaling frame:", err.Error())
 			}
-			// send it now (flush)
-			//TODO optimize; only flush if no packets waiting on input
-			if err = outPort.Writer.Flush(); err != nil {
-				fmt.Fprintln(os.Stderr, "ERROR: flushing netout:", err)
+			// only flush if no packets waiting on input
+			if buffered == 0 {
+				// send it now (flush)
+				if err = outPort.Writer.Flush(); err != nil {
+					fmt.Fprintln(os.Stderr, "ERROR: flushing netout:", err)
+				}
 			}
 		}
 	}
