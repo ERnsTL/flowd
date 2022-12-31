9cf44fc8231e0c572e8e0ed7961c4d032108c1a6
Ernst Rohlicek <ernst@rohlicek.at>
2022-12-31 12:39:34 +0100

    flowd-rs: Add basic count component impl (closes #252)

--- flowd-rs/src/main.rs
+++ flowd-rs/src/main.rs
@@ -80,6 +80,7 @@ fn main() {
         FileReaderComponent::get_metadata(),
         TrimComponent::get_metadata(),
         SplitLinesComponent::get_metadata(),
+        CountComponent::get_metadata(),
     ])));
     //TODO actually load components
     info!("component library initialized");
@@ -1681,6 +1682,7 @@ impl RuntimeRuntimePayload {
                     "FileReader" => { FileReaderComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
                     "Trim" => { TrimComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
                     "SplitLines" => { SplitLinesComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
+                    "Count" => { CountComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
                     _ => {
                         error!("unknown component in network start! exiting thread.");
                     }
@@ -6269,4 +6271,109 @@ impl Component for SplitLinesComponent {
     }
 }
 
+struct CountComponent {
+    inn: ProcessEdgeSource,
+    out: ProcessEdgeSink,
+    signals_in: ProcessSignalSource,
+    signals_out: ProcessSignalSink,
+}
+
+impl Component for CountComponent {
+    fn new(mut inports: ProcessInports, mut outports: ProcessOutports, signals_in: ProcessSignalSource, signals_out: ProcessSignalSink) -> Self where Self: Sized {
+        CountComponent {
+            inn: inports.remove("IN").expect("found no IN inport"),
+            out: outports.remove("OUT").expect("found no OUT outport"),
+            signals_in: signals_in,
+            signals_out: signals_out,
+        }
+    }
+
+    fn run(mut self) {
+        debug!("Count is now run()ning!");
+        let inn = &mut self.inn;
+        let out = &mut self.out.sink;
+        let out_wakeup = self.out.wakeup.as_ref().unwrap();
+        let mut packets: usize = 0;
+        let mut now = chrono::Utc::now();
+        loop {
+            trace!("begin of iteration");
+            // check for shutdown of input -> send final report
+            if inn.is_abandoned() {
+                // send final report
+                debug!("got EOF on input, sending final report");
+                out.push(format!("{}", packets).into_bytes()).expect("could not push into OUT");   //TODO optimize https://docs.rs/itoa/latest/itoa/
+                out_wakeup.unpark();
+                debug!("done");
+            }
+
+            // check signals
+            if let Ok(ip) = self.signals_in.try_recv() {
+                trace!("received signal ip: {}", str::from_utf8(&ip).expect("invalid utf-8"));
+                // stop signal
+                if ip == b"stop" {   //TODO optimize comparison
+                    info!("got stop signal, exiting");
+                    break;
+                } else if ip == b"ping" {
+                    trace!("got ping signal, responding");
+                    self.signals_out.send(b"pong".to_vec());
+                } else {
+                    warn!("received unknown signal ip: {}", str::from_utf8(&ip).expect("invalid utf-8"))
+                }
+            }
+            // check in port
+            //TODO add reset port
+            //TODO add triggered report by sending something into REPORT port
+            //TODO add ability to forward as well (output count on separate port?)
+            //TODO add counting of packet sizes, certain metadata etc.
+            loop {
+                if let Ok(ip) = inn.pop() {
+                    // count packet
+                    packets += 1;
+                } else {
+                    break;
+                }
+            }
+            //TODO detect EOF on in port
+            if packets % 1000000 == 0 {
+                info!("got 1M packets, duration: {}", chrono::Utc::now() - now);
+            }
+            trace!("-- end of iteration");
+            thread::park();
+        }
+        info!("exiting");
+    }
+
+    fn get_metadata() -> ComponentComponentPayload where Self: Sized {
+        ComponentComponentPayload {
+            name: String::from("Count"),
+            description: String::from("Counts the number of packets, discarding them, and sending the packet count every 1M packets."), //TODO
+            icon: String::from("cut"),
+            subgraph: false,
+            in_ports: vec![
+                ComponentPort {
+                    name: String::from("IN"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("IPs to count"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            out_ports: vec![
+                ComponentPort {
+                    name: String::from("OUT"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("reports count on this outport"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            ..Default::default()
+        }
+    }
 }
