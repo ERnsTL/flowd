3befae58aa4305da25473d9a362867404bdee5e1
Ernst Rohlicek <ernst@rohlicek.at>
2022-12-31 12:37:41 +0100

    flowd-rs: Add trim component (closes #250)

--- flowd-rs/src/main.rs
+++ flowd-rs/src/main.rs
@@ -78,6 +78,7 @@ fn main() {
         LibComponent::get_metadata(),
         UnixSocketServerComponent::get_metadata(),
         FileReaderComponent::get_metadata(),
+        TrimComponent::get_metadata(),
     ])));
     //TODO actually load components
     info!("component library initialized");
@@ -1677,6 +1678,7 @@ impl RuntimeRuntimePayload {
                     "LibComponent" => { LibComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
                     "UnixSocketServer" => { UnixSocketServerComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
                     "FileReader" => { FileReaderComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
+                    "Trim" => { TrimComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
                     _ => {
                         error!("unknown component in network start! exiting thread.");
                     }
@@ -6050,4 +6052,107 @@ impl Component for FileReaderComponent {
             ..Default::default()
         }
     }
+}
+
+struct TrimComponent {
+    inn: ProcessEdgeSource,
+    out: ProcessEdgeSink,
+    signals_in: ProcessSignalSource,
+    signals_out: ProcessSignalSink,
+}
+
+impl Component for TrimComponent {
+    fn new(mut inports: ProcessInports, mut outports: ProcessOutports, signals_in: ProcessSignalSource, signals_out: ProcessSignalSink) -> Self where Self: Sized {
+        TrimComponent {
+            inn: inports.remove("IN").expect("found no IN inport"),
+            out: outports.remove("OUT").expect("found no OUT outport"),
+            signals_in: signals_in,
+            signals_out: signals_out,
+        }
+    }
+
+    fn run(mut self) {
+        debug!("Trim is now run()ning!");
+        let inn = &mut self.inn;    //TODO optimize
+        let out = &mut self.out.sink;
+        let out_wakeup = self.out.wakeup.as_ref().unwrap();
+        loop {
+            trace!("begin of iteration");
+            // check signals
+            if let Ok(ip) = self.signals_in.try_recv() {
+                trace!("received signal ip: {}", str::from_utf8(&ip).expect("invalid utf-8"));
+                // stop signal
+                if ip == b"stop" {   //TODO optimize comparison
+                    info!("got stop signal, exiting");
+                    break;
+                } else if ip == b"ping" {
+                    trace!("got ping signal, responding");
+                    self.signals_out.send(b"pong".to_vec());
+                } else {
+                    warn!("received unknown signal ip: {}", str::from_utf8(&ip).expect("invalid utf-8"))
+                }
+            }
+            // check in port
+            loop {
+                if let Ok(ip) = inn.pop() {
+                    // read packet - expecting UTF-8 string
+                    let mut text = str::from_utf8(&ip).expect("non utf-8 data");
+                    debug!("got a text to trim: {}", &text);
+
+                    // trim string
+                    debug!("len before trim: {}", text.len());
+                    text = text.trim();
+                    debug!("len after trim: {}", text.len());
+
+                    // send it
+                    debug!("forwarding trimmed string...");
+                    //TODO optimize .to_vec() copies the contents - is Vec::from faster?
+                    out.push(Vec::from(text)).expect("could not push into OUT");
+                    out_wakeup.unpark();
+                    debug!("done");
+                } else {
+                    break;
+                }
+            }
+            trace!("-- end of iteration");
+            thread::park();
+        }
+        info!("exiting");
+    }
+
+    fn get_metadata() -> ComponentComponentPayload where Self: Sized {
+        ComponentComponentPayload {
+            name: String::from("Trim"),
+            description: String::from("Reads IPs as UTF-8 strings and trims whitespace at beginning and end, forwarding the trimmed string."),
+            icon: String::from("cut"),
+            subgraph: false,
+            in_ports: vec![
+                ComponentPort {
+                    name: String::from("IN"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("IPs with strings to trim, one string per IP"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            out_ports: vec![
+                ComponentPort {
+                    name: String::from("OUT"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("trimmed strings"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            ..Default::default()
+        }
+    }
+}
+
 }
