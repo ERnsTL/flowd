c4e67e15d82cbd4fed53a7ed3fac0224cf6e0c81
Ernst Rohlicek <ernst@rohlicek.at>
2022-12-31 12:35:06 +0100

    flowd-rs: Add file read component (closes #249)

--- flowd-rs/src/main.rs
+++ flowd-rs/src/main.rs
@@ -77,6 +77,7 @@ fn main() {
         OutputComponent::get_metadata(),
         LibComponent::get_metadata(),
         UnixSocketServerComponent::get_metadata(),
+        FileReaderComponent::get_metadata(),
     ])));
     //TODO actually load components
     info!("component library initialized");
@@ -1675,6 +1676,7 @@ impl RuntimeRuntimePayload {
                     "Output" => { OutputComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
                     "LibComponent" => { LibComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
                     "UnixSocketServer" => { UnixSocketServerComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
+                    "FileReader" => { FileReaderComponent::new(inports, outports, signalsource, caretaker_signalsink_clone).run(); },
                     _ => {
                         error!("unknown component in network start! exiting thread.");
                     }
@@ -5943,5 +5945,109 @@ type ProcessManager = HashMap<String, Process>;
 impl std::fmt::Debug for Process {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Process").field("signal", &self.signal).field("joinhandle", &self.joinhandle).finish()
+    }
+}
+
+struct FileReaderComponent {
+    inn: ProcessEdgeSource,
+    out: ProcessEdgeSink,
+    signals_in: ProcessSignalSource,
+    signals_out: ProcessSignalSink,
+}
+
+impl Component for FileReaderComponent {
+    fn new(mut inports: ProcessInports, mut outports: ProcessOutports, signals_in: ProcessSignalSource, signals_out: ProcessSignalSink) -> Self where Self: Sized {
+        FileReaderComponent {
+            inn: inports.remove("NAMES").expect("found no NAMES inport"),
+            out: outports.remove("OUT").expect("found no OUT outport"),
+            signals_in: signals_in,
+            signals_out: signals_out,
+        }
+    }
+
+    fn run(mut self) {
+        debug!("FileReader is now run()ning!");
+        let filenames = &mut self.inn;    //TODO optimize
+        let out = &mut self.out.sink;
+        let out_wakeup = self.out.wakeup.as_ref().unwrap();
+        loop {
+            trace!("begin of iteration");
+            // check signals
+            //TODO optimize, there is also try_recv() and recv_timeout()
+            if let Ok(ip) = self.signals_in.try_recv() {
+                //TODO optimize string conversions
+                trace!("received signal ip: {}", str::from_utf8(&ip).expect("invalid utf-8"));
+                // stop signal
+                if ip == b"stop" {   //TODO optimize comparison
+                    info!("got stop signal, exiting");
+                    break;
+                } else if ip == b"ping" {
+                    trace!("got ping signal, responding");
+                    self.signals_out.send(b"pong".to_vec());
+                } else {
+                    warn!("received unknown signal ip: {}", str::from_utf8(&ip).expect("invalid utf-8"))
+                }
+            }
+            // check in port
+            //TODO while !inn.is_empty() {
+            loop {
+                if let Ok(ip) = filenames.pop() {
+                    // read filename on inport
+                    let file_path = str::from_utf8(&ip).expect("non utf-8 data");
+                    debug!("got a filename: {}", &file_path);
+
+                    // read whole file
+                    //TODO may be big file - add chunking
+                    //TODO enclose files in brackets to know where its stream of chunks start and end
+                    debug!("reading file...");
+                    let contents = std::fs::read(file_path).expect("should have been able to read the file");
+
+                    // send it
+                    debug!("forwarding file contents...");
+                    out.push(contents).expect("could not push into OUT");
+                    out_wakeup.unpark();
+                    debug!("done");
+                } else {
+                    break;
+                }
+            }
+            trace!("-- end of iteration");
+            thread::park();
+        }
+        info!("exiting");
+    }
+
+    fn get_metadata() -> ComponentComponentPayload where Self: Sized {
+        ComponentComponentPayload {
+            name: String::from("FileReader"),
+            description: String::from("Reads the contents of the given files and sends the contents."),
+            icon: String::from("file"),
+            subgraph: false,
+            in_ports: vec![
+                ComponentPort {
+                    name: String::from("NAMES"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("filenames, one per IP"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            out_ports: vec![
+                ComponentPort {
+                    name: String::from("OUT"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("conents of the given files"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            ..Default::default()
+        }
     }
 }
