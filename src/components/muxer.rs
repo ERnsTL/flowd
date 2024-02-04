177f32b94263e837f74d55bcfc06adbb08eecd08
Ernst Rohlicek <ernst@rohlicek.at>
2024-02-04 19:47:05 +0100

    MuxerComponent: Add multiplexer component, a derivative of Repeat supporting multiple inports (#216)

---
+++ src/components/muxer.rs
@@ -0,0 +1,106 @@
+use std::sync::{Arc, Mutex};
+use crate::{ProcessEdgeSource, ProcessEdgeSink, Component, ProcessSignalSink, ProcessSignalSource, GraphInportOutportHolder, ProcessInports, ProcessOutports, ComponentComponentPayload, ComponentPort};
+
+pub struct MuxerComponent {
+    inn: ProcessEdgeSource,
+    out: ProcessEdgeSink,
+    signals_in: ProcessSignalSource,
+    signals_out: ProcessSignalSink,
+    //graph_inout: Arc<Mutex<GraphInportOutportHolder>>,
+}
+
+impl Component for MuxerComponent {
+    fn new(mut inports: ProcessInports, mut outports: ProcessOutports, signals_in: ProcessSignalSource, signals_out: ProcessSignalSink, _graph_inout: Arc<Mutex<GraphInportOutportHolder>>) -> Self where Self: Sized {
+        MuxerComponent {
+            inn: inports.remove("IN").expect("found no IN inport").pop().unwrap(),
+            out: outports.remove("OUT").expect("found no OUT outport").pop().unwrap(),
+            signals_in: signals_in,
+            signals_out: signals_out,
+            //graph_inout: graph_inout,
+        }
+    }
+
+    fn run(mut self) {
+        debug!("Muxer is now run()ning!");
+        let inn = &mut self.inn;    //TODO optimize these references, not really needed for them to be referenes, can just consume?
+        let out = &mut self.out.sink;
+        let out_wakeup = self.out.wakeup.expect("got no wakeup handle for outport OUT");
+        loop {
+            trace!("begin of iteration");
+
+            // check signals
+            //TODO optimize, there is also try_recv() and recv_timeout()
+            if let Ok(ip) = self.signals_in.try_recv() {
+                //TODO optimize string conversions
+                trace!("received signal ip: {}", std::str::from_utf8(&ip).expect("invalid utf-8"));
+                // stop signal
+                if ip == b"stop" {   //TODO optimize comparison
+                    info!("got stop signal, exiting");
+                    break;
+                } else if ip == b"ping" {
+                    trace!("got ping signal, responding");
+                    self.signals_out.send(b"pong".to_vec()).expect("could not send pong");
+                }
+            }
+
+            // check in port
+            loop {
+                if let Ok(ip) = inn.pop() {
+                    debug!("repeating packet...");
+                    out.push(ip).expect("could not push into OUT");
+                    out_wakeup.unpark();
+                    debug!("done");
+                } else {
+                    break;
+                }
+            }
+
+            // are we done?
+            if inn.is_abandoned() {
+                // input closed, nothing more to do
+                info!("EOF on inport, shutting down");
+                drop(out);
+                out_wakeup.unpark();
+                break;
+            }
+
+            trace!("-- end of iteration");
+            std::thread::park();
+        }
+        info!("exiting");
+    }
+
+    fn get_metadata() -> ComponentComponentPayload where Self: Sized {
+        ComponentComponentPayload {
+            name: String::from("Muxer"),
+            description: String::from("Copies data as-is from IN port(s) to single OUT port."),
+            icon: String::from("dedent"),
+            subgraph: false,
+            in_ports: vec![
+                ComponentPort {
+                    name: String::from("IN"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: true,
+                    description: String::from("IPs to be multiplexed into outport"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            out_ports: vec![
+                ComponentPort {
+                    name: String::from("OUT"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("multiplexed IPs from IN port"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            ..Default::default()
+        }
+    }
+}
