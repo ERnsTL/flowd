269ba6c72dc27309ac9962b35eef3286a77a99f1
Ernst Rohlicek <ernst@rohlicek.at>
2024-03-31 17:00:36 +0200

    unixsocketclient: Add Unix socket client component impl (WIP) (#303)

---
+++ src/components/unixsocketclient.rs
@@ -0,0 +1,139 @@
+use std::sync::{Arc, Mutex};
+use crate::{ProcessEdgeSource, ProcessEdgeSink, Component, ProcessSignalSink, ProcessSignalSource, GraphInportOutportHolder, ProcessInports, ProcessOutports, ComponentComponentPayload, ComponentPort};
+
+pub struct UnixSocketClientComponent {
+    conf: ProcessEdgeSource,
+    inn: ProcessEdgeSource,
+    out: ProcessEdgeSink,
+    signals_in: ProcessSignalSource,
+    signals_out: ProcessSignalSink,
+    //graph_inout: Arc<Mutex<GraphInportOutportHolder>>,
+}
+
+impl Component for UnixSocketClientComponent {
+    fn new(mut inports: ProcessInports, mut outports: ProcessOutports, signals_in: ProcessSignalSource, signals_out: ProcessSignalSink, _graph_inout: Arc<Mutex<GraphInportOutportHolder>>) -> Self where Self: Sized {
+        UnixSocketClientComponent {
+            conf: inports.remove("CONF").expect("found no CONF inport").pop().unwrap(),
+            inn: inports.remove("IN").expect("found no IN inport").pop().unwrap(),
+            out: outports.remove("OUT").expect("found no OUT outport").pop().unwrap(),
+            signals_in: signals_in,
+            signals_out: signals_out,
+            //graph_inout: graph_inout,
+        }
+    }
+
+    fn run(self) {
+        debug!("UnixSocketClient is now run()ning!");
+        debug!("TextReplace is now run()ning!");
+        let mut conf = self.conf;
+        let mut inn = self.inn;
+        let mut out = self.out.sink;
+        let out_wakeup = self.out.wakeup.expect("got no wakeup handle for outport OUT");
+
+        // read configuration
+        trace!("read config IPs");
+        /*
+        while conf.is_empty() {
+            thread::yield_now();
+        }
+        */
+        let Ok(file_name) = conf.pop() else { trace!("no config IP received - exiting"); return; };
+
+        // configure
+        // NOTE: nothing to be done here
+
+        // main loop
+        loop {
+            trace!("begin of iteration");
+
+            // check signals
+            if let Ok(ip) = self.signals_in.try_recv() {
+                //TODO optimize string conversions
+                trace!("received signal ip: {}", std::str::from_utf8(&ip).expect("invalid utf-8"));
+                // stop signal
+                if ip == b"stop" {   //TODO optimize comparison
+                    info!("got stop signal, exiting");
+                    break;
+                } else if ip == b"ping" {
+                    trace!("got ping signal, responding");
+                    self.signals_out.send(b"pong".to_vec()).expect("could not send pong");
+                } else {
+                    warn!("received unknown signal ip: {}", std::str::from_utf8(&ip).expect("invalid utf-8"))
+                }
+            }
+
+            // check in port
+            /*
+            loop {
+                if let Ok(_ip) = inn.pop() {
+                    debug!("got a packet, dropping it.");
+                } else {
+                    break;
+                }
+            }
+            */
+            while !inn.is_empty() {
+                //_ = inn.pop().ok();
+                //debug!("got a packet, dropping it.");
+
+                debug!("got {} packets, dropping them.", inn.slots());  //###
+                inn.read_chunk(inn.slots()).expect("receive as chunk failed").commit_all();
+            }
+
+            // are we done?
+            if inn.is_abandoned() {
+                info!("EOF on inport, shutting down");
+                //out_wakeup.unpark();
+                break;
+            }
+
+            trace!("-- end of iteration");
+            std::thread::park();
+        }
+        info!("exiting");
+    }
+
+    fn get_metadata() -> ComponentComponentPayload where Self: Sized {
+        ComponentComponentPayload {
+            name: String::from("UnixSocketClient"),
+            description: String::from("Drops all packets received on IN port."),    //###
+            icon: String::from("trash-o"),  //###
+            subgraph: false,
+            in_ports: vec![
+                ComponentPort {
+                    name: String::from("CONF"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("data to be dropped"),    //###
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                },
+                ComponentPort {
+                    name: String::from("IN"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("data to be dropped"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            out_ports: vec![
+                ComponentPort {
+                    name: String::from("OUT"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("data to be dropped"),    //###
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            ..Default::default()
+        }
+    }
+}
