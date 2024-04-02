afcff9d27496f73e00ecdc195bbc25c064112730
Ernst Rohlicek <ernst@rohlicek.at>
2024-04-02 22:18:52 +0200

    ws: Add WebSocket client component (#307)

---
+++ src/components/ws.rs
@@ -0,0 +1,179 @@
+use std::sync::{Arc, Mutex};
+use crate::{ProcessEdgeSource, ProcessEdgeSink, Component, ProcessSignalSink, ProcessSignalSource, GraphInportOutportHolder, ProcessInports, ProcessOutports, ComponentComponentPayload, ComponentPort};
+
+// component-specific
+//use tungstenite::client::connect;
+use tungstenite::protocol::Message;
+use std::time::Duration;
+
+pub struct WSClientComponent {
+    conf: ProcessEdgeSource,
+    inn: ProcessEdgeSource,
+    out: ProcessEdgeSink,
+    signals_in: ProcessSignalSource,
+    signals_out: ProcessSignalSink,
+    //graph_inout: Arc<Mutex<GraphInportOutportHolder>>,
+}
+
+const READ_TIMEOUT: Duration = Duration::from_millis(500);
+
+impl Component for WSClientComponent {
+    fn new(mut inports: ProcessInports, mut outports: ProcessOutports, signals_in: ProcessSignalSource, signals_out: ProcessSignalSink, _graph_inout: Arc<Mutex<GraphInportOutportHolder>>) -> Self where Self: Sized {
+        WSClientComponent {
+            conf: inports.remove("CONF").expect("found no CONF inport").pop().unwrap(),
+            inn: inports.remove("IN").expect("found no IN inport").pop().unwrap(),
+            out: outports.remove("OUT").expect("found no OUT outport").pop().unwrap(),
+            signals_in: signals_in,
+            signals_out: signals_out,
+            //graph_inout: graph_inout,
+        }
+    }
+
+    fn run(self) {
+        debug!("WSClient is now run()ning!");
+        let mut conf = self.conf;
+        let mut inn = self.inn;
+        let mut out = self.out.sink;
+        let out_wakeup = self.out.wakeup.expect("got no wakeup handle for outport OUT");
+
+        // read configuration
+        trace!("read config IPs");
+        /*
+        while conf.is_empty() {
+            thread::yield_now();
+        }
+        */
+        let Ok(url_vec) = conf.pop() else { error!("no config IP received - exiting"); return; };
+
+        // prepare connection arguments
+        // NOTE: currently no options implemented
+        let url_str = std::str::from_utf8(&url_vec).expect("invalid utf-8");
+        //let url = url::Url::parse(&url_str).expect("failed to parse URL");
+        //let mut query_pairs = url.query_pairs();
+        //TODO optimize ^ re-use the query_pairs iterator? wont find anything after first .find() call
+        // get buffer size from URL
+        /*
+        let _read_buffer_size;
+        if let Some((_key, value)) = url.query_pairs().find(|(key, _)| key == "rbuffer") {
+            _read_buffer_size = value.to_string().parse::<usize>().expect("failed to parse query pair value for read buffer as integer");
+        } else {
+            _read_buffer_size = DEFAULT_READ_BUFFER_SIZE;
+        }
+        */
+
+        // configure
+        let (mut client, _) = tungstenite::client::connect(url_str).expect("failed to connect to WebSocket server");    //TODO hande response
+
+        // main loop
+        loop {
+            trace!("begin of iteration");
+
+            // check signals
+            if let Ok(ip) = self.signals_in.try_recv() {
+                //TODO optimize string conversions
+                trace!("received signal ip: {}", std::str::from_utf8(&ip).expect("invalid utf-8"));
+                // stop signal
+                if ip == b"stop" {   //TODO optimize comparison
+                    info!("got stop signal, exiting");
+                    break;
+                } else if ip == b"ping" {
+                    trace!("got ping signal, responding");
+                    self.signals_out.send(b"pong".to_vec()).expect("could not send pong");
+                } else {
+                    warn!("received unknown signal ip: {}", std::str::from_utf8(&ip).expect("invalid utf-8"))
+                }
+            }
+
+            // send
+            /*
+            loop {
+                if let Ok(_ip) = inn.pop() {
+                    debug!("got a packet, dropping it.");
+                } else {
+                    break;
+                }
+            }
+            */
+            while !inn.is_empty() {
+                debug!("got {} packets, sending into WebSocket...", inn.slots());
+                let chunk = inn.read_chunk(inn.slots()).expect("receive as chunk failed");
+
+                for ip in chunk.into_iter() {
+                    //TODO add support for Text messages
+                    client.write(Message::Binary(ip)).unwrap(); //TODO handle Result - but it is only () anyway
+                }
+
+                client.flush().expect("failed to flush WebSocket client");
+            }
+
+            // receive
+            //TODO optimize - better to send or receive first? and send|receive first on FBP or on socket?
+            while client.can_read() {
+                debug!("got message from WebSocket, repeating...");
+                let msg = client.read().expect("failed to read from WebSocket");
+                out.push(msg.into_data()).expect("could not push into OUT");
+                out_wakeup.unpark();
+                debug!("done");
+            }
+
+            // are we done?
+            if inn.is_abandoned() {
+                info!("EOF on inport, shutting down");
+                //TODO close socket
+                drop(out);
+                out_wakeup.unpark();
+                break;
+            }
+
+            trace!("-- end of iteration");
+            //TODO optimize - hacky way to avoid busy waiting
+            //std::thread::park();
+            std::thread::sleep(READ_TIMEOUT);
+        }
+        info!("exiting");
+    }
+
+    fn get_metadata() -> ComponentComponentPayload where Self: Sized {
+        ComponentComponentPayload {
+            name: String::from("WSClient"),
+            description: String::from("Sends and receives messages, transformed into IPs, via a WebSocket connection."),
+            icon: String::from("plug"),
+            subgraph: false,
+            in_ports: vec![
+                ComponentPort {
+                    name: String::from("CONF"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("connection URL with optional configuration parameters in query part - currently none defined"),
+                    values_allowed: vec![],
+                    value_default: String::from("wss://example.com:8080/socketpath")
+                },
+                ComponentPort {
+                    name: String::from("IN"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("incoming WebSocket messages, transformed to IPs"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            out_ports: vec![
+                ComponentPort {
+                    name: String::from("OUT"),
+                    allowed_type: String::from("any"),
+                    schema: None,
+                    required: true,
+                    is_arrayport: false,
+                    description: String::from("IPs to be sent as WebSocket messages"),
+                    values_allowed: vec![],
+                    value_default: String::from("")
+                }
+            ],
+            ..Default::default()
+        }
+    }
+}
