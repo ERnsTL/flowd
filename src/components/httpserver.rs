45e7c4095b6e64297f7969db7662a8bea53eecd5
Ernst Rohlicek <ernst@rohlicek.at>
2024-04-03 18:58:53 +0200

    httpserver: Change imports to standard format of all components

--- src/components/httpserver.rs
+++ src/components/httpserver.rs
@@ -1,7 +1,8 @@
-use std::{borrow::BorrowMut, sync::{Arc, Mutex}};
+use std::sync::{Arc, Mutex};
 use crate::{ProcessEdgeSource, ProcessEdgeSink, Component, ProcessSignalSink, ProcessSignalSource, GraphInportOutportHolder, ProcessInports, ProcessOutports, ComponentComponentPayload, ComponentPort};
 
 // component-specific
+use std::borrow::BorrowMut;
 use std::thread::{self};
 use astra::{Body, Request, Response, Server};
 
