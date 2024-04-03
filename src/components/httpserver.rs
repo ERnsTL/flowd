a47999e37f0e23a9662cd8cfe8306d79a91fd764
Ernst Rohlicek <ernst@rohlicek.at>
2024-04-03 19:02:31 +0200

    httpserver: Remove empty source file

--- src/components/httpserver.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::{Arc, Mutex};
-use crate::{ProcessEdgeSource, ProcessEdgeSink, Component, ProcessSignalSink, ProcessSignalSource, GraphInportOutportHolder, ProcessInports, ProcessOutports, ComponentComponentPayload, ComponentPort};
-
-// component-specific
-use std::borrow::BorrowMut;
-use std::thread::{self};
-use astra::{Body, Request, Response, Server};
-
