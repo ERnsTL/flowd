d2e3e2c97008adeb66df6c964285b5838ec383e9
Ernst Rohlicek <ernst@rohlicek.at>
2024-04-03 18:56:45 +0200

    unixsocket: Rename unixsocketclient to unixsocket, remove unixsocketserver

--- src/components/unixsocketserver.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::{Arc, Mutex};
-use crate::{ProcessEdgeSource, ProcessEdgeSink, Component, ProcessSignalSink, ProcessSignalSource, GraphInportOutportHolder, ProcessInports, ProcessOutports, ComponentComponentPayload, ComponentPort};
-
-// component-specific
-use std::io::{Write, Read};
-use std::thread::{self};
-use std::collections::HashMap;
-
