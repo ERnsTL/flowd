64034cb45ce9f9bcc8517ce17f0106f15a7b8a33
Ernst Rohlicek <ernst@rohlicek.at>
2024-04-08 19:17:54 +0200

    ssh: Add SSH client component (WIP) (#306)

--- src/components/ssh.rs
+++ src/components/ssh.rs
@@ -1,12 +1,12 @@
 use std::sync::{Arc, Mutex};
+use std::vec;
 use crate::{ProcessEdgeSource, ProcessEdgeSink, Component, ProcessSignalSink, ProcessSignalSource, GraphInportOutportHolder, ProcessInports, ProcessOutports, ComponentComponentPayload, ComponentPort};
 
 //component-specific
-use ssh2::Session;
-use std::ffi::{OsStr,OsString};
-use std::process::{Command, Stdio};
-use std::io::{BufRead, BufReader, Error, ErrorKind, Write};
+use std::ffi::OsString;
 use lexopt::prelude::*;
+use ssh::TerminalSize;
+use std::time::Duration;
 
 /*
 Ability to remotely execute a command and forward data to its STDIN and receive data from its STDOUT.
@@ -27,6 +27,9 @@ pub struct SSHClientComponent {
     signals_out: ProcessSignalSink,
     //graph_inout: Arc<Mutex<GraphInportOutportHolder>>,
 }
+
+const CONNECT_TIMEOUT: Option<Duration> = Some(Duration::from_secs(7));
+const READ_WRITE_TIMEOUT: Option<Duration> = Some(Duration::from_millis(500));
 
 enum Mode { One, Each }
 
@@ -60,29 +63,70 @@ impl Component for SSHClientComponent {
             let cmd_args1: Vec<String> = cmd_words.drain(1..).collect();
             cmd_args = cmd_args1.iter().map(|x| OsString::from(x)).collect();
         }
-        let cmd_program1 = cmd_words.pop().expect("could not pop program name");
-        let cmd_program = OsStr::new(cmd_program1.as_str());
-        debug!("got program {} with arguments {:?}", cmd_program.to_str().expect("could not convert OsStr to &str"), cmd_args);
+        let cmd_program = cmd_words.pop().expect("could not pop program name");
+        debug!("got program {} with arguments {:?}", cmd_program, cmd_args);
 
         // read configuration
+        let mut username: Option<String> = None;
+        let mut password: Option<String> = None;
+        let mut private_key_path: Option<String> = None;
         let mut mode = Mode::Each;
         let mut retry = false;
+        let mut pipe_out: bool = false;
         let mut parser = lexopt::Parser::from_args(vec![OsString::from(std::str::from_utf8(&conf.pop().expect("could not read IP from CONF configuration inport")).expect("invalid utf-8"))]);
+        let mut address_port: Option<String> = None;
         while let Some(arg) = parser.next().expect("could not call next()") {
             match arg {
                 Long("retry") => {
-                    retry = parser.value().expect("could not get parser value").parse().expect("could not parse value");
+                    retry = parser.value().expect("failed to get value for retry").parse().expect("failed to parse value for retry");
                 }
                 Long("mode") => {
-                    let mode_str: OsString = parser.value().expect("could not get parser value").parse::<OsString>().expect("could not parse value");
+                    let mode_str: OsString = parser.value().expect("failed to get value for mode").parse::<OsString>().expect("failed to parse value for mode");
                     match mode_str.to_str().expect("could not convert mode_str to str") {
                         "one" => { mode = Mode::One; },
                         "each" => { mode = Mode::Each; },
                         _ => { unreachable!(); }
                     }
                 }
+                Long("user") => {
+                    username = Some(parser.value().expect("failed to get value for user").into_string().expect("failed to get parser value for user"));
+                }
+                Long("pass") => {
+                    password = Some(parser.value().expect("failed to get value for pass").into_string().expect("failed to get parser value for pass"));
+                }
+                Short('i') => {
+                    private_key_path = Some(parser.value().expect("failed to get value for -i").into_string().expect("failed to get parser value for -i"));
+                }
+                Long("pipeout") => {
+                    pipe_out = parser.value().expect("failed to get value for pipeout").parse().expect("failed to parse value for pipeout");
+                }
+                Value(val) => {
+                    // check for address:port
+                    if address_port.is_none() {
+                        address_port = Some(val.into_string().expect("could not convert address_port to string"));
+                    } else {
+                        warn!("ignoring extra value: {:?}", val);
+                    }
+                }
                 _ => { unreachable!(); }
             }
+        }
+        // check configuration
+        if address_port.is_none() {
+            error!("missing address:port as free argument - exiting");
+            return;
+        }
+        let address_port = address_port.unwrap();
+        //TODO implement
+        if retry {
+            unimplemented!("retry not implemented yet");
+        }
+        match mode {
+            Mode::One => {
+                error!("mode one not implemented yet - exiting");
+                return;
+            }
+            Mode::Each => {}
         }
 
         // main loop
@@ -111,91 +155,71 @@ impl Component for SSHClientComponent {
                     debug!("got a packet, starting sub-process:");
                     //println!("{}", std::str::from_utf8(&ip).expect("non utf-8 data")); //TODO optimize avoid clone here
 
-                    //###
-                    let mut session = ssh::create_session()
-                        .username("ubuntu")
-                        .password("password")
-                        .private_key_path("./id_rsa")
-                        .connect("127.0.0.1:22")
+                    // build session
+                    let mut session_builder = ssh::create_session();
+                    if let Some(ref username) = username {
+                        session_builder = session_builder.username(&username);
+                    };
+                    if let Some(ref password) = password {
+                        session_builder = session_builder.password(&password);
+                    };
+                    if let Some(ref private_key_path) = private_key_path {
+                        session_builder = session_builder.private_key_path(private_key_path);
+                    };
+                    session_builder = session_builder.timeout(READ_WRITE_TIMEOUT);
+
+                    // open session
+                    let mut session = session_builder
+                        .connect_with_timeout(&address_port, CONNECT_TIMEOUT)   //TODO optimize - avoid unwrap, could do unwrap_unchecked because of previous check
                         .unwrap()
                         .run_local();
-                    let exec = session.open_exec().unwrap();
-                    let vec: Vec<u8> = exec.send_command("ls -all").unwrap();
-                    println!("{}", String::from_utf8(vec).unwrap());
 
-                    let mut exec = session.open_exec().unwrap();
-                    exec.exec_command("no_command").unwrap();
-                    let vec = exec.get_output().unwrap();
-                    println!("output: {}", String::from_utf8(vec).unwrap());
-                    println!("exit status: {}", exec.exit_status().unwrap());
-                    println!("terminated msg: {}", exec.terminate_msg().unwrap());
-                    let _ = exec.close();
+                    // run remote command
+                    //TODO funtional overlap with mode = one
+                    let out_vec: Vec<u8>;
+                    if pipe_out {   // stream data from SSH command to next component
+                        // open and execute command in SSH channel in session
+                        let channel = session.open_channel().unwrap();
+                        // start shell in the channel
+                        let mut channel_shell = channel.shell(TerminalSize::from_type(80, 25, ssh::TerminalSizeType::Character)).unwrap();
+                        // write trigger IP into the remote shell STDIN
+                        channel_shell.write(&ip).expect("failed to write into channel_shell");
+                        // read from that command
+                        out_vec = channel_shell.read().expect("failed to read from channel_shell");
+                        //TODO implement this mode - currently does only 1 read()
+                    } else { // one command execution per inport packet
+                        // capture of output desired
+                        let exec = session.open_exec().expect("failed to open exec");
+                        out_vec = exec.send_command(&cmd_program).expect("failed to send command");
 
-                    // Close session.
-                    session.close();
-
-                    //TODO this runs the sub-process but for longer-running or hanging processes the Cmd component is unresponsive for signals
-                    //FIXME be responsive during longer-running sub-processes or for server sub-processes
-                    //TODO add ability to send signal to sub-process (HUP, KILL, TERM etc.)
-                    // NOTE: Alternative to IFS for reading all of STDIN:
-                    //   input=$(cat)
-                    //let mut child = Command::new("bash")
-                    //    .args(["-c", "IFS= read -r -d '' input ; echo stdin is \"$input\" ; a=1 ; while [ true ] ; do echo bla$a; sleep 2s ; ((a=a+1)) ; done"])
-                    //let mut child = Command::new("recsel")
-                    //    .args(["-p","name", "/dev/shm/test.rec"])
-                    //let mut child = Command::new("bash")
-                    //    .args(["-c", "nc -l -n 127.0.0.1 8080"])    // NOTE: adding a grep or similar has its own buffering so you will not see immediate output on child STDOUT
-                    match mode {
-                        Mode::Each => {
-                            let mut child = Command::new(cmd_program)   //TODO optimize pre-construct Command once
-                                .args(&cmd_args)    // NOTE: adding a grep or similar has its own buffering so you will not see immediate output on child STDOUT
-                                .stdin(Stdio::piped())
-                                .stdout(Stdio::piped())
-                                .spawn()
-                                .expect("could not start sub-process");
-                            if retry {
-                                //TODO implement
-                                unimplemented!();
-                            }
-
-                            // set up reader from child STDOUT
-                            let reader = BufReader::new(child
-                                .stdout
-                                .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))
-                                .expect("could not get standard output")    //TODO optimize looks like duplication from above line
-                            );
-                            // set up writer into child STDIN
-                            let mut writer = child
-                                .stdin
-                                //.ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard input."))
-                                //.expect("could not get standard input")    //TODO optimize looks like duplication from above line
-                                .take()
-                                .expect("could not get standard input");
-
-                            // deliver the trigger IP contents into child STDIN
-                            writer.write(ip.as_slice()).expect("could not write into child STDIN");
-                            drop(writer);   // close STDIN
-
-                            // read child STDOUT until closed
-                            reader
-                                .lines()
-                                .filter_map(|line| line.ok())
-                                //.filter(|line| line.find("usb").is_some())
-                                //.for_each(|line| println!("{}", line));
-                                .for_each(|line| {
-                                    //debug!("repeating packet...");
-                                    out.push(line.into_bytes()).expect("could not push into OUT");
-                                    out_wakeup.unpark();
-                                });
-                        },
-                        Mode::One => { unimplemented!(); }  //TODO implement
+                        // no capture of output needed
+                        //TODO make configurable
+                        //TODO deliver the trigger IP contents to the sub-process STDIN, if possible
+                        /*
+                        let mut exec = session.open_exec().unwrap();
+                        exec.exec_command("no_output_command").unwrap();
+                        out_vec = exec.get_output().unwrap();
+                        println!("exit status: {}", exec.exit_status().unwrap());
+                        println!("terminated msg: {}", exec.terminate_msg().unwrap());
+                        let _ = exec.close();   //TODO optimize - reuse?
+                        */
                     }
+
+                    debug!("repeating remote process output...");
+                    out.push(out_vec).expect("could not push into OUT");
+                    out_wakeup.unpark();
+                    
+                    // close session
+                    //TODO optimize - reuse?
+                    session.close();
 
                     debug!("done");
                 } else {
                     break;
                 }
             }
+
+            // are we done yet?
             if inn.is_abandoned() {
                 info!("EOF on inport, shutting down");
                 drop(out);
@@ -212,7 +236,7 @@ impl Component for SSHClientComponent {
     fn get_metadata() -> ComponentComponentPayload where Self: Sized {
         ComponentComponentPayload {
             name: String::from("SSHClient"),
-            description: String::from("Runs an external program and forwards STDIN, STDERR and STDOUT."),   //###
+            description: String::from("Runs a program remotely and forwards STDIN, STDERR and STDOUT."),
             icon: String::from("terminal"),
             subgraph: false,
             //TODO config inport - flag mode operating mode: one (command instance handling all IPs) or each (IP handled by new instance)
@@ -225,7 +249,7 @@ impl Component for SSHClientComponent {
                     schema: None,
                     required: true,
                     is_arrayport: false,
-                    description: String::from("data to be sent to the sub-process STDIN"),
+                    description: String::from("data to be sent to the remote process STDIN"),
                     values_allowed: vec![],
                     value_default: String::from("")
                 },
@@ -235,7 +259,7 @@ impl Component for SSHClientComponent {
                     schema: None,
                     required: true,
                     is_arrayport: false,
-                    description: String::from("POSIX shell-compatible path and arguments for the sub-process"),
+                    description: String::from("POSIX shell-compatible path and arguments for the remote process"),
                     values_allowed: vec![],
                     value_default: String::from("")
                 },
@@ -257,7 +281,7 @@ impl Component for SSHClientComponent {
                     schema: None,
                     required: true,
                     is_arrayport: false,
-                    description: String::from("STDOUT output data coming from the sub-process"),
+                    description: String::from("STDOUT output data coming from the remote process"),
                     values_allowed: vec![],
                     value_default: String::from("")
                 }
